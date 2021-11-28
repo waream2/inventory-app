c39c6a4dbb9d6512f8e6660f859b2f577b319aea
earn <waream2@gmail.com>
2021-11-27 23:29:06 -0500

    config: preparing for Heroku deploy

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var categoriesRouter = require('./routes/category')
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const categoriesRouter = require('./routes/category');
 
-var app = express();
+const app = express();
 
-//Set up mongoose connection
-var mongoose = require('mongoose');
-var dev_db_url = 'mongodb+srv://waream2:zxcxz@cluster0.egnm9.mongodb.net/test'
-var mongoDB = dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-var db = mongoose.connection;
+// Set up mongoose connection
+const mongoose = require('mongoose');
+
+const dev_db_url = 'mongodb+srv://waream2:zxcxz@cluster0.egnm9.mongodb.net/test';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -35,19 +34,19 @@ app.use('/users', usersRouter);
 app.use('/category', categoriesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use((req, res, next) => {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
